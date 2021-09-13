b63a4d052705e5def3c7f497bebfe5ac5093453f
NormalPlayerJSH <jsh010904@gmail.com>
2021-09-13 15:59:12 +0900

    프론트 공통 버튼, 입력창 제작

---
+++ frontend/src/Pages/Common/TextInput/TextInput.tsx
@@ -0,0 +1,36 @@
+import React, { ChangeEvent, ReactElement, useState } from "react";
+import styles from "./TextInput.module.css";
+
+function useTextInput(_setting?: {
+  initialValue?: string;
+  innerText?: string;
+  className?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}): [ReactElement, string] {
+  const checkDefault = <T,>(toCheck: T | undefined, defaultValue: T) => {
+    if (toCheck) return toCheck;
+    return defaultValue;
+  };
+  const setting = checkDefault(_setting, {});
+  const initialValue = checkDefault(setting.initialValue, "");
+  const [TextValue, setTextValue] = useState(initialValue);
+  const innerText = checkDefault(setting.innerText, "");
+  const className = checkDefault(setting.className, "");
+  const paramOnChange = checkDefault(setting.onChange, () => {});
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTextValue(event.target.value);
+    paramOnChange(event);
+  };
+  return [
+    <input
+      type="text"
+      placeholder={innerText}
+      className={`${className} ${styles.inputElem}`}
+      value={TextValue}
+      onChange={onChange}
+    />,
+    TextValue,
+  ];
+}
+
+export default useTextInput;
