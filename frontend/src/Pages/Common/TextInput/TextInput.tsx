1705a6a9527b68f2153443a6d2018d4297b54faf
NormalPlayerJSH <jsh010904@gmail.com>
2021-09-13 17:26:22 +0900

    프론트 텍스트 인풋 애니메이션 수정

--- frontend/src/Pages/Common/TextInput/TextInput.tsx
+++ frontend/src/Pages/Common/TextInput/TextInput.tsx
@@ -1,4 +1,9 @@
-import React, { ChangeEvent, ReactElement, useState } from "react";
+import React, {
+  ChangeEvent,
+  ReactElement,
+  useState,
+  KeyboardEvent,
+} from "react";
 import styles from "./TextInput.module.css";
 
 function useTextInput(_setting?: {
@@ -6,6 +11,7 @@ function useTextInput(_setting?: {
   innerText?: string;
   className?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onEnter?: (event: KeyboardEvent<HTMLInputElement>) => void;
 }): [ReactElement, string] {
   const checkDefault = <T,>(toCheck: T | undefined, defaultValue: T) => {
     if (toCheck) return toCheck;
@@ -17,9 +23,15 @@ function useTextInput(_setting?: {
   const innerText = checkDefault(setting.innerText, "");
   const className = checkDefault(setting.className, "");
   const paramOnChange = checkDefault(setting.onChange, () => {});
+  const paramOnEnter = checkDefault(setting.onEnter, () => {});
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTextValue(event.target.value);
     paramOnChange(event);
+  };
+  const onEnter = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      paramOnEnter(event);
+    }
   };
   return [
     <input
@@ -28,6 +40,7 @@ function useTextInput(_setting?: {
       className={`${className} ${styles.inputElem}`}
       value={TextValue}
       onChange={onChange}
+      onKeyPress={onEnter}
     />,
     TextValue,
   ];
