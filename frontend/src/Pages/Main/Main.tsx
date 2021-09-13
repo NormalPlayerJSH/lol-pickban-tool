ec8abf05e9528cef4f3b1445e5d38007205d9ffb
NormalPlayerJSH <jsh010904@gmail.com>
2021-09-13 17:26:33 +0900

    프론트 메인 화면 구성

--- frontend/src/Pages/Main/Main.tsx
+++ frontend/src/Pages/Main/Main.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import { RouteComponentProps } from "react-router";
 import NavBar from "../NavBar/NavBar";
 import common from "../common.module.css";
+import Button from "../Common/Button/Button";
+import styles from "./Main.module.css";
 
 function Main(RCProps: RouteComponentProps) {
+  const move = (link: string) => {
+    RCProps.history.push(link);
+  };
   return (
     <>
       <NavBar RCProps={RCProps} from="main" />
       <div className={common.div}>
         <div className={common.inner}>
           <div className={common.mainTitle}>LOLGO Banpick Tool</div>
+          <Button className={styles.btn} onClick={() => move("/create")}>
+            밴픽 방 만들기
+          </Button>
+          <Button className={styles.btn} onClick={() => move("/join")}>
+            밴픽 방 입장하기
+          </Button>
         </div>
       </div>
     </>
