c63462f997902d151df4fd4f3283269d51bca18d
NormalPlayerJSH <jsh010904@gmail.com>
2021-09-13 13:41:27 +0900

    프론트 옵저버 모드 설정

--- frontend/src/Pages/Banpick/Banpick.tsx
+++ frontend/src/Pages/Banpick/Banpick.tsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { io } from "socket.io-client";
 import { getSocket } from "../../Middle/socket";
-import Test from "./test";
-import useSWR from "swr";
 import { useBanpickSWR } from "../../Middle/useBanpickSWR";
 import BanpickWindow from "./BanpickWindow/BanpickWindow";
 
 function Banpick(RCProps: RouteComponentProps<{ code: string }>) {
   const code = RCProps.match.params.code;
   const { socket, emitter } = getSocket();
-  const { banpickData, sessionData, banpickMutate, sessionMutate } =
-    useBanpickSWR();
+  const { sessionData, banpickMutate, sessionMutate } = useBanpickSWR();
   useEffect(() => {
     socket.on("joinSuccess", (data) => {
       const { info, ans } = data;
@@ -28,19 +24,6 @@ function Banpick(RCProps: RouteComponentProps<{ code: string }>) {
     return <div>로딩중</div>;
   }
   return <BanpickWindow />;
-
-  return (
-    <div>
-      <div
-        onClick={() => {
-          emitter.join(code);
-        }}
-      >
-        Banpick Component {`${JSON.stringify(sessionData)}`}
-      </div>
-      <Test />
-    </div>
-  );
 }
 
 export default Banpick;
--- frontend/src/Pages/Banpick/BanpickWindow/BanpickWindow.tsx
+++ frontend/src/Pages/Banpick/BanpickWindow/BanpickWindow.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ScoreBoard from "./ScoreBoard";
 import BanPick from "./BanPick/BanPick";
 import SelectArea from "./SelectArea/SelectArea";
+import { useBanpickSWR } from "../../../Middle/useBanpickSWR";
 
 function BanpickWindow() {
+  const { sessionData } = useBanpickSWR();
   return (
     <div className="App">
       <ScoreBoard></ScoreBoard>
       <div className="banpicks">
         <BanPick team={"BLUE"}></BanPick>
-        <SelectArea></SelectArea>
+        {sessionData !== "OBSERVER" ? <SelectArea></SelectArea> : <></>}
         <BanPick team={"RED"}></BanPick>
       </div>
     </div>
